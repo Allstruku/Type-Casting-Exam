958692e762c8feac5c08fbb9f9b2f1b9ea724b00
Allstruku <86404980+Allstruku@users.noreply.github.com>
2021-07-08 15:55:46 +0900

    Update typeCastingExam.java

--- src/typeCastingExam.java
+++ src/typeCastingExam.java
@@ -5,12 +5,13 @@ public class typeCastingExam {
 		long y = x;
 		System.out.println(y);
 		
-		int x2 = 6;
-		long y2 = x2;
-		System.out.println(y2);
+//		int x2 = 6;
+//		long y2 = x2;
+//		System.out.println(y2);
+		// 더 큰 데이터타입에서 작은 데이터타입으로는 옮기면 컴파일러에서 오류가 생김 
 		
 		int x3 = 5;
-		long y3 = x3;
+		long y3 = (int) x3;
 		System.out.println(y3);
 	}
 
