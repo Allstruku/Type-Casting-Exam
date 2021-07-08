7d4733cc015d7efa79947f75f07d874bfd01f1f7
ParkJooahn <ParkJooahn@localhost>
2021-07-08 15:37:00 +0900

    Initial Commit

---
+++ src/typeCastingExam.java
@@ -0,0 +1,17 @@
+
+public class typeCastingExam {
+	public static void main(String[] args) {
+		int x = 50000;
+		long y = x;
+		System.out.println(y);
+		
+		int x2 = 6;
+		long y2 = x2;
+		System.out.println(y2);
+		
+		int x3 = 5;
+		long y3 = x3;
+		System.out.println(y3);
+	}
+
+}
